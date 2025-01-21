abd433f8afa5d76858c5e30f16550f5ac488b9cd
Julia Mari Bernaus <jumaber@gmail.com>
2025-01-21 21:04:31 +0100

    Solution

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Data Types:
 /*******************************************
     Iteration 1.1 | Tongue Twister
 *******************************************/
@@ -9,9 +10,11 @@ const s5 = "and";
 
 // Concatenate the string variables into one new string
 
+let tongueTwister = `${s1} ${s2} ${s3} ${s4} ${s5} ${s3} ${s2} ${s1} ${s4}`;
 
 // Print out the concatenated string
 
+console.log(tongueTwister);
 
 
 
@@ -22,11 +25,12 @@ const part1 = "java";
 const part2 = "script";
 
 // Convert the last letter of part1 and part2 to uppercase and concatenate the strings
-
+let upperCased1 = part1.slice(0,3) + part1.charAt(3).toUpperCase();
+let upperCased2 = part2.slice(0,5) + part2.charAt(5).toUpperCase();
+let parts = upperCased1 + upperCased2;
 
 // Print the cameLtaiL-formatted string
-
-
+console.log(parts);
 
 
 /*******************************************
@@ -35,10 +39,10 @@ const part2 = "script";
 const billTotal = 84;
 
 // Calculate the tip (15% of the bill total)
-
+const tip = (billTotal/ 100) * 15;
 
 // Print out the tipAmount
-
+console.log(tip)
 
 
 
@@ -47,10 +51,12 @@ const billTotal = 84;
 *******************************************/
 
 // Generate a random integer between 1 and 10 (inclusive)
-
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 // Print the generated random number
-
+console.log(getRandomInt(1, 10));
 
 
 /*******************************************
@@ -62,15 +68,29 @@ const b = false;
 
 // Try and guess the output of the below expressions first and write your answers down:
 const expression1 = a && b;
+// false
+console.log(expression1);
 
 const expression2 = a || b;
+// true 
+console.log(expression2);
 
 const expression3 = !a && b;
+// false
+console.log(expression3);
 
 const expression4 = !(a && b);
+// true
+console.log(expression4);
 
 const expression5 = !a || !b;
+// true
+console.log(expression5);
 
 const expression6 = !(a || b);
+// false 
+console.log(expression6);
 
 const expression7 = a && a;
+// true
+console.log(expression7);
